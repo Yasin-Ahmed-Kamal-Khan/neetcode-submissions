3d2e9b50474bd2d9e3fd4c1fc2440086b1747fd3
Yasin Khan <109461320+Yasin-Ahmed-Kamal-Khan@users.noreply.github.com>
2026-08-07 18:00:23 +0100

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.py
@@ -0,0 +1,21 @@
+        # You are given an integer array heights where heights[i] represents the height of the ith bar.
+
+# You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+# 
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        left = 0
+        right = len(heights) - 1
+        maxArea = 0
+        while left < right:
+            width = right - left
+            height = min(heights[left], heights[right])
+            area = width * height
+            maxArea = max(maxArea, area)
+
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return maxArea
